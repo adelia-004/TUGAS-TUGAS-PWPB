49da20bebd23eb8830c8bb528991d1004e3719b2
aprilliadw-004 <aprilliachyadw@gmail.com>
2021-11-03 11:55:21 +0800

    tambah folder

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,9 +1,15 @@
 <?php
 
-class Home {
+class Home extends Controller {
     public function index() {
-        echo "Home/index";
+        $data['judul'] = "Home";
+        $this->view('templates/header', $data);
+        $this->view('home/index');
+        $this->view('templates/footer');
+        
     }
+
 }
+
 
 ?>
